5143e949ff14f0735f7e4b52dd9a752904d280ec
D3annyC <ch.chung127@gmail.com>
2018-07-18 11:05:43 +0800

    modfied ps code

--- app.py
+++ app.py
@@ -15,24 +15,6 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('ecJBWIBQJ6RH7/SAo5VqFJFh1RfBkooN2HiwevskFRuznCa8frGzWY/hDNqpGoqsC2m3WmwLL/MxzFZiBgGzVOol1U1oWfYG0u4JUl8cme7O9Y/0vJ4ag2tQUSn1pKdSGBivuyeeyjnZyVpx3y+EzwdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('81c68c012b7345ca6098fa3225924525')
 
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text='Hello, world')
