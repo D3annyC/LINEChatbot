23f810d6f461c70594c9e56b498db8def1fc6307
D3annyC <ch.chung127@gmail.com>
2018-07-18 10:58:42 +0800

    modfy code

--- app.py
+++ app.py
@@ -33,12 +33,13 @@ def callback():
 
     return 'OK'
 
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    message = TextSendMessage(text='Hello, world')
+    line_bot_api.reply_message(event.reply_token, message)
+
+
+
 
 
 if __name__ == "__main__":
